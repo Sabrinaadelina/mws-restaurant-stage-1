46459eb18b274f5268348dfe4fd2ebd0e3e69fbf
Sabrinaadelina <37504469+Sabrinaadelina@users.noreply.github.com>
2018-08-09 18:53:34 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -144,10 +144,10 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.src = DBHelper.imageUrlForRestaurant(restaurant) alt="An image ofrestaurant name";
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
